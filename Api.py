3364578b4ee1d3e48ad75592e506d2b1bf35e81d
AleEsp76 <56982261+AleEsp76@users.noreply.github.com>
2022-05-08 19:25:46 -0600

    third commit

--- Api.py
+++ Api.py
@@ -1,15 +1,16 @@
+#Libraries and packages that are used within the script
 from cmath import e
 import csv
 import json
 from logging import raiseExceptions
 import pandas as pd
-
 from operator import index
-
 import requests
 
+#URL used to fetch information from the API
 url = "https://www.cradlepointecm.com/api/v2/routers/?state=online&account__in=92725&fields=account,id,ipv4_address,description,state"
 
+#Permissions required to access the API
 headers = {
     'X-CP-API-ID': 'a2fb9d1d',
     'X-CP-API-KEY': '608f3a84ae2e1666b81c2757b7f98252',
@@ -20,7 +21,7 @@ headers = {
 
 routers = []
 
-
+#Function used to obtain information from routers
 def get_routers():
     Exceptions = []
     req = requests.get(url, headers=headers)
@@ -33,6 +34,7 @@ def get_routers():
             if "city" in router_location_data:
                 router["city"] = router_location_data["city"]
             else:
+                #If it doesn't find the required information it will write an error message to a .txt file
                 raise Exception("Error, couldn´t find city for router with id: "+router["id"])
             if "state" in router_location_data:
                 router["state_location"] = router_location_data["state"]
@@ -42,12 +44,12 @@ def get_routers():
             log_file_error(str(e))
     save_data_csv(routers_response)
 
-
+#This function reads the description string that comes from the router and takes only the zip code that is found by separating the text by commas
 def get_zip_code_from_router(router):
     description: str = router["description"]
     zip_code = description.split(', ')[1]
     return zip_code
-    
+#Esta funcion lee el archivo cvs donde se encuentran la informacion de la ubicacion de los routers utilizando de parametro de busqueda el codigo zip    
 def get_state_and_city_from_router(router):
     file = open("US_GeoStats.csv", "r")
     csvreader = csv.reader(file)
@@ -57,16 +59,17 @@ def get_state_and_city_from_router(router):
             state = row[2]
             return {"city": city, "state": state}
     return {}
-    
+
+#This function takes the information from the routers, opens a .cvs file and saves the information.
 def save_data_csv(routers):
     file = open("routers.csv", "w")
     routers_to_string = json.dumps(routers)
     df = pd.read_json (routers_to_string)
     df.to_csv (file, index = None)
 
-
+#This function receives an error string, opens a .txt file and writes the error that was found in it
 def log_file_error(error):
-    file = open("error.txt", "a")
+    file = open("RouterStateErrors.txt", "a")
     file.write(error)
     file.write('\n')
 
--- READ_ME.txt
+++ READ_ME.txt
@@ -1,33 +1,20 @@
 # README
 
-## Autor
+## Author
+Alejandro Espinoza Morales, 118140284.
+Year: 2022
 
-<ul>
-<li>Alejandro Espinoza Morales, 118140284.</li>
-</ul>
-</p>
+## Program Requirements:
 
-Año: 2022
+-Python 3.7: For the installation of python we will enter the official python download page https://www.python.org/downloads/windows//
 
-## Preparacion:
+-Have pip installed.
 
-Algunos programas que se necesitan para compilar este programa son:
-
--Python: para la instalacion de python entraremos a la pagina oficial de descargas de python https://www.python.org/downloads/windows/
-y descargar el instalador de la version mas reciente para el sistema operativo deseado. 
-
--Visual Studio Code: para descargar este programa iremos a la pagina oficial de descarga de visual studio code y descargamos la version 
-del programa para el sistema operativo deseado.
-
--requests: Para instalar requests es necesario tener python, ya que necesitaremos el instalador de paquetes pip(el cual se incluye en la 
-instalación de python) y para poder conseguir requests entraremos al cmd de la computadora y escribiremos "pip install requests"
-
--pandas: Para instalar pandas es necesario tener python, ya que necesitaremos el instalador de paquetes pip(el cual se incluye en la 
-instalación de python) y para poder conseguir requests entraremos al cmd de la computadora y escribiremos "pip install pandas"
+-Run the commmand "make install_requirements" in the terminal, this is to install all the necessary dependencies. 
 
 ## Manual de Uso
 
-Este proyecto cumple con la funcion de obtener informacion de una API a partir de un URL modificado y de un archivo cvs externo y 
-guardarla en un archivo .cvs, asi como crear un archivo .txt cuando detecta algun error y anotar el error. Para poder ingresar a 
-la API se deben de colocar correctamente los permisos de entrada si no la API no regresara ninguna informacion.
+This project fulfills the function of obtaining information from an API from a modified URL and an external cvs file and
+save it in a .cvs file, as well as create a .txt file when it detects an error and write down the error. In order to enter
+The API must correctly set the input permissions, otherwise the API will not return any information.
 
